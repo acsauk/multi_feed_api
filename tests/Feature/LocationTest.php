40057d98c7203c987feeed5a3a47dc198f259f68
Alex Saunders <acsauk@gmail.com>
2018-03-18 18:23:55 +0000

    creates failing test for location creation via locations#store

--- tests/Feature/LocationTest.php
+++ tests/Feature/LocationTest.php
@@ -35,4 +35,48 @@ class LocationTest extends TestCase
             'image' => $location->image,
         ]);
     }
+
+    /** @test */
+    public function location_is_created_via_json_payload()
+    {
+        // Arrange
+        // Create values for JSON payload
+        $name = 'Central Park';
+        $latitude = '40.784083425938';
+        $longitude = '-73.964853286743';
+        $address = '59th St to 110th St (5th Ave to Central Park West)';
+        $category = 'Park';
+        $link = 'https://foursquare.com/v/412d2800f964a520df0c1fe3';
+        $rating = '9.8';
+        $image = 'https://igx.4sqi.net/img/general/500x500/655018_Zp3vA90Sy4IIDApvfAo5KnDItoV0uEDZeST7bWT-qzk.jpg';
+
+        $payload = [
+          'name' => $name,
+          'latitude' => $latitude,
+          'longitude' => $longitude,
+          'address' => $address,
+          'category' => $category,
+          'link' => $link,
+          'rating' => $rating,
+          'image' => $image
+        ];
+
+        // Act
+        // Post JSON payload to locations#store route
+        $this->json('post', '/api/locations', $payload)
+        // Assert
+        // Check the location was created (201)
+            ->assertStatus(201)
+        // and details returned are correct
+            ->assertJson([
+              'name' => $name,
+              'latitude' => $latitude,
+              'longitude' => $longitude,
+              'address' => $address,
+              'category' => $ategory,
+              'link' => $link,
+              'rating' => $rating,
+              'image' => $image
+            ]);
+    }
 }
