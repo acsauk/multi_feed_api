e21a33887415e829ea749bbfed4eeeca7482ca6b
Alex Saunders <acsauk@gmail.com>
2018-03-18 19:04:49 +0000

    creates failing test for having a nullable price property of Location

--- tests/Feature/LocationTest.php
+++ tests/Feature/LocationTest.php
@@ -79,4 +79,51 @@ class LocationTest extends TestCase
               'image' => $image
             ]);
     }
+
+    /** @test */
+    public function location_can_be_created_with_null_price()
+    {
+        // Arrange
+        // Create values for JSON payload
+        $name = 'Central Park';
+        $latitude = '40.784083425938';
+        $longitude = '-73.964853286743';
+        $address = '59th St to 110th St (5th Ave to Central Park West)';
+        $category = 'Park';
+        $link = 'https://foursquare.com/v/412d2800f964a520df0c1fe3';
+        $rating = '9.8';
+        $image = 'https://igx.4sqi.net/img/general/500x500/655018_Zp3vA90Sy4IIDApvfAo5KnDItoV0uEDZeST7bWT-qzk.jpg';
+        $price = null;
+
+        $payload = [
+          'name' => $name,
+          'latitude' => $latitude,
+          'longitude' => $longitude,
+          'address' => $address,
+          'category' => $category,
+          'link' => $link,
+          'rating' => $rating,
+          'image' => $image,
+          'price' => null
+        ];
+
+        // Act
+        // Post JSON payload to locations#store route
+        $this->json('post', '/api/locations', $payload)
+        // Assert
+        // Check the location was created (201)
+            ->assertStatus(201)
+        // and details returned are correct
+            ->assertJson([
+              'name' => $name,
+              'latitude' => $latitude,
+              'longitude' => $longitude,
+              'address' => $address,
+              'category' => $category,
+              'link' => $link,
+              'rating' => $rating,
+              'image' => $image,
+              'price' => null
+            ]);
+    }
 }
